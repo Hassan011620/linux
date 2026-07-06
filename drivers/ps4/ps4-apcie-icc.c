012c33c20d00f62b3b37593e138987d27f65202a
rmuxnet <armandas.kvietkus@proton.me>
2026-07-06 19:23:49 +0200

    ps4-apcie-icc: fix register_chrdev success misread as failure

--- drivers/ps4/ps4-apcie-icc.c
+++ drivers/ps4/ps4-apcie-icc.c
@@ -645,7 +645,7 @@ int apcie_icc_init(struct apcie_dev *sc)
 	}
 
 	ret = register_chrdev(0, "icc", &icc_fops);
-	if (ret) {
+	if (ret < 0) {
 		sc_warn("icc: register_chrdev failed: %d\n", ret);
 		kfree(ioctl_reply_buf);
 		kfree(ioctl_send_buf);
