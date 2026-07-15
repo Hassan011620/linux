187ee0c05f2c866a11fabf3ae4250564924c6142
rmux <armandas.kvietkus@proton.me>
2026-07-15 17:14:18 +0200

    ps4-fan: remove RPM support and unused header defines

--- drivers/ps4/ps4-fan.h
+++ drivers/ps4/ps4-fan.h
@@ -6,25 +6,17 @@
 #define PS4_FAN_ICC_MAJOR               0x0A
 #define PS4_FAN_ICC_MINOR_GET           0x07
 #define PS4_FAN_ICC_MINOR_SET           0x06
-#define PS4_FAN_ICC_MINOR_STATUS        0x08
 
 #define PS4_FAN_TEMP_ICC_MAJOR          0x0B
 #define PS4_FAN_TEMP_ICC_MINOR          0x01
 
 #define PS4_FAN_CONFIG_LEN              0x34
 #define PS4_FAN_CONFIG_REPLY_LEN        0x52
-#define PS4_FAN_STATUS_REPLY_LEN        0x52
 #define PS4_FAN_TEMP_REPLY_LEN          0x10
 
 #define PS4_ICC_STATUS_BYTE             0
 #define PS4_FAN_THRESH_BYTE             5
-#define PS4_FAN_RPM_OFFSET              8
-#define PS4_FAN_TEMP_ALT_OFFSET         16
 #define PS4_FAN_TEMP_BYTE               3
-
-#define PS4_FAN_RPM_SCALE               65536U
-#define PS4_FAN_RPM_INVALID_1           0xFFFFFFFFU
-#define PS4_FAN_RPM_INVALID_2           0x0FFFFFFFU
 
 #define PS4_FAN_THRESH_MIN_C            20
 #define PS4_FAN_THRESH_MAX_C            85
