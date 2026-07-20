c78e412793ef5ef7ea0c273974bb4b6d60dadb87
Armandas Kvietkus <armandas.kvietkus@proton.me>
2026-07-20 12:20:44 +0000

    amdgpu/ps4_bridge: drop MSI 1080p100 and VGA 720p quirks

--- drivers/gpu/drm/amd/amdgpu/ps4_bridge_quirks.h
+++ drivers/gpu/drm/amd/amdgpu/ps4_bridge_quirks.h
@@ -5,8 +5,6 @@
 #include <drm/drm_edid.h>
 
 #define PS4_BRIDGE_QUIRK_MODE_1080P60	BIT(0)
-#define PS4_BRIDGE_QUIRK_MODE_1080P100	BIT(1)
-#define PS4_BRIDGE_QUIRK_MODE_720P	BIT(2)
 
 unsigned int ps4_bridge_quirk_forced_modes(const struct edid *edid);
 
