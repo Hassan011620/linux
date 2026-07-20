0a0d36936aecc3f89f8653d0cc833375943d7f33
Armandas Kvietkus <armandas.kvietkus@proton.me>
2026-07-20 11:56:35 +0000

    amdgpu/ps4_bridge: support multi-mode quirks, MSI 1080p100

--- drivers/gpu/drm/amd/amdgpu/ps4_bridge_quirks.c
+++ drivers/gpu/drm/amd/amdgpu/ps4_bridge_quirks.c
@@ -7,21 +7,22 @@
 struct ps4_bridge_monitor_quirk {
 	u16 manufacturer_name;
 	u16 product_code;
-	enum ps4_bridge_forced_mode forced_mode;
+	unsigned int forced_modes;
 };
 
 static const struct ps4_bridge_monitor_quirk ps4_bridge_monitor_quirks[] = {
-	{ 0x4a8b, 0x1366, PS4_BRIDGE_FORCED_MODE_1080P },
-	{ 0x5a63, 0x8a31, PS4_BRIDGE_FORCED_MODE_720P },
+	{ 0x4a8b, 0x1366, PS4_BRIDGE_QUIRK_MODE_1080P60 },
+	{ 0x5a63, 0x8a31, PS4_BRIDGE_QUIRK_MODE_720P },
+	{ 0x3669, 0x40b5, PS4_BRIDGE_QUIRK_MODE_1080P60 | PS4_BRIDGE_QUIRK_MODE_1080P100 },
 };
 
-enum ps4_bridge_forced_mode ps4_bridge_quirk_forced_mode(const struct edid *edid)
+unsigned int ps4_bridge_quirk_forced_modes(const struct edid *edid)
 {
 	u16 mfg, prod;
 	size_t i;
 
 	if (!edid)
-		return PS4_BRIDGE_FORCED_MODE_NONE;
+		return 0;
 
 	mfg = be16_to_cpu(edid->product_id.manufacturer_name);
 	prod = le16_to_cpu(edid->product_id.product_code);
@@ -29,8 +30,8 @@ enum ps4_bridge_forced_mode ps4_bridge_quirk_forced_mode(const struct edid *edid
 	for (i = 0; i < ARRAY_SIZE(ps4_bridge_monitor_quirks); i++) {
 		if (ps4_bridge_monitor_quirks[i].manufacturer_name == mfg &&
 		    ps4_bridge_monitor_quirks[i].product_code == prod)
-			return ps4_bridge_monitor_quirks[i].forced_mode;
+			return ps4_bridge_monitor_quirks[i].forced_modes;
 	}
 
-	return PS4_BRIDGE_FORCED_MODE_NONE;
+	return 0;
 }
