39946f79aba9f6cbe9bbebedc28fc725379728f1
Armandas Kvietkus <armandas.kvietkus@proton.me>
2026-07-20 10:44:42 +0000

    amdgpu/ps4_bridge: add 720p-only quirk, VSC monitor

--- drivers/gpu/drm/amd/amdgpu/ps4_bridge_quirks.h
+++ drivers/gpu/drm/amd/amdgpu/ps4_bridge_quirks.h
@@ -3,6 +3,12 @@
 
 #include <drm/drm_edid.h>
 
-bool ps4_bridge_quirk_force_1080p(const struct edid *edid);
+enum ps4_bridge_forced_mode {
+	PS4_BRIDGE_FORCED_MODE_NONE,
+	PS4_BRIDGE_FORCED_MODE_1080P,
+	PS4_BRIDGE_FORCED_MODE_720P,
+};
+
+enum ps4_bridge_forced_mode ps4_bridge_quirk_forced_mode(const struct edid *edid);
 
 #endif
