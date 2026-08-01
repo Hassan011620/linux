23452d6356bee668b30e81bc8192b2a459a9b129
rmuxnet <armandas.kvietkus@proton.me>
2026-08-01 15:17:00 +0200

    Revert "ps4/icc: add boot-time EDID dump test over ICC i2c"
    This reverts commit fb717d7b186c945458873c84e3e948a915350721.

--- drivers/ps4/icc/edid_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PS4_ICC_EDID_TEST_H__
-#define __PS4_ICC_EDID_TEST_H__
-
-struct apcie_dev;
-
-void icc_edid_dump_test(struct apcie_dev *sc);
-
-#endif
--- drivers/ps4/ps4-apcie-icc.c
+++ drivers/ps4/ps4-apcie-icc.c
@@ -8,7 +8,6 @@
 #include <linux/uaccess.h>
 #include <asm/ps4.h>
 #include "aeolia.h"
-#include "icc/edid_test.h"
 
 /* There should normally be only one Aeolia device in a system. This allows
  * other kernel code in unrelated subsystems to issue icc requests without
@@ -638,9 +637,7 @@ int apcie_icc_init(struct apcie_dev *sc)
 		sc_err("icc: i2c init failed: %d\n", ret);
 		goto unassign_global;
 	}
-
-	icc_edid_dump_test(sc);
-
+	
 	resetBtWlan();
 //	resetUsbPort();
 	
